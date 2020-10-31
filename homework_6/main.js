07df8037cb09fc0ac301a271a240d98748a3ed2c
divmohan7 <60532598+divmohan7@users.noreply.github.com>
2020-10-30 23:49:38 -0400

    buttons

--- homework_6/main.js
+++ homework_6/main.js
@@ -11,7 +11,7 @@ function priceCounter(){
 	price.innerHTML =  document.getElementById("quantity").value;
 }
 function setImage(select){
-  var image = document.getElementsByName("image-swap")[0];
+  var image = document.getElementByName("image-swap")[0];
   image.src = select.options[select.selectedIndex].value;
 }  
 
