5208dc82447316e2462e91ac16e263c986ef44b2
divmohan7 <60532598+divmohan7@users.noreply.github.com>
2020-11-08 18:56:50 -0500

    6B initial edits

--- homework_6/main.js
+++ homework_6/main.js
@@ -43,6 +43,7 @@ function addToCart() {
 
 //creates array to hold bag items
 var productArr = []
+var productArr2 = []
 
 class Product {
 	
@@ -67,4 +68,43 @@ function addToCart() {
 function updateCartNumber(num) {
 	var cartCount = document.getElementById('quantityCount')
 	cartCount.innerHTML = num
+}
+function goToCheckoutPage() {
+	alert("taking you to your cart")
+	localStorage.setItem('order', JSON.stringify(productArr))
+
+	window.location.replace("cart.html")
+}
+function checkoutPageLoaded() {
+	var loadedProductArr = localStorage.getItem('order')
+	var productArr2 = JSON.parse(loadedProductArr)
+	console.log(productArr2)
+
+	var cart = document.getElementById("cart")
+
+	for(var i = 0; i < productArr2.length; i++) {
+		var cinbun = productArr2[i]
+		var cinbunGlaze = cinbun.glaze
+		var cinbunQuantity = cinbun.quantity
+
+		cart.innerHTML += "<div class='cart-items'> Flavor: Pumpkin Spice Glaze: <img src="+ cinbunGlaze +"> Price: "+ cinbunQuantity +"</div>"
+		cart.innerHTML += "<span onclick= 'deleteProduct(' + i + ')'> Delete </span>"
+		
+	}
+}
+function deleteProduct(i) {
+	alert("i : " + i)
+	
+	productArr2.splice(i,1)
+	
+	cart.innerHTML = ''
+
+	for(var i = 0; i < productArr2.length; i++) {
+		var cinbun = productArr2[i]
+		var cinbunGlaze = cinbun.glaze
+		var cinbunQuantity = cinbun.quantity
+	
+		cart.innerHTML += "<div class='cart-items'> Flavor: Pumpkin Spice Glaze: <img src="+ cinbunGlaze +"> Price: "+ cinbunQuantity +"</div>"
+		cart.innerHTML += "<span onclick= 'deleteProduct(' + i + ')'> Delete </span>"
+	}
 }
