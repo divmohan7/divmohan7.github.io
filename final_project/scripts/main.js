92e0ba73a69bf3848eda620430c7a8cbff926a40
divmohan7 <60532598+divmohan7@users.noreply.github.com>
2020-12-11 16:08:19 -0500

    css updates

--- final_project/scripts/main.js
+++ final_project/scripts/main.js
@@ -1,17 +1,20 @@
 new fullpage('#fullpage', {
+    licenseKey: 'null',
     menu: '#timeline',
     navigation: true,
     navigationPosition: 'left',
 	lockAnchors: true,
-	anchors:['early90s', 'late90s', 'early00s', 'late00s', 'today'],
+	anchors:['early-90s', 'late-90s', 'early-00s', 'late-00s', 'present'],
 	autoScrolling:true,
     scrollHorizontally: true,
     verticalCentered: true,
-    sectionsColor: ['transparent', 'teal', 'lavender', 'lightgreen', 'transparent'],
     dragAndMove: true,
     fixedElements: ['#moveSectionDown','#moveSectionUp'],
     paddingTop: '50px',
     paddingBottom: '50px',
+    fitToSection: true,
+    navigationTooltips: ['Home', 'The Early 1990s', 'The Late 1990s', 'The Early 2000s', 'The Late 2000s', 'The Web Today'],
+    showActiveTooltip: true,
 });
 document.querySelector('#moveSectionUp').addEventListener('click', function(e){
     e.preventDefault();
